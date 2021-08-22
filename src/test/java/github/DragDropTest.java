4695809c4fffa3b211844b6cd52b0db098e9acf5
Roman Golovin <romagolovin@gmail.com>
2021-08-23 00:06:49 +0500

    final commit

--- src/test/java/github/DragDropTest.java
+++ src/test/java/github/DragDropTest.java
@@ -1,4 +1,25 @@
 package github;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
 public class DragDropTest {
+    @BeforeAll
+    static void setup() {
+        Configuration.baseUrl = "https://the-internet.herokuapp.com";
+        Configuration.startMaximized = true;
+    }
+    @Test
+    void dragndropTest(){
+    open("/drag_and_drop");
+    $("#column-a").dragAndDropTo($("#column-b"));
+    $("#column-b").shouldHave(text("A"));
+
+    sleep(5000);
+    }
+
 }
